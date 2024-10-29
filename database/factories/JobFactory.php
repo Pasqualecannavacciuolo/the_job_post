c9d2357c830404f1de5526614257126dad3832c8
Pasqualecannavacciuolo <p.cannavacciuolo@icloud.com>
2024-10-29 15:52:58 +0100

    Aggiunti Seeder&Factory

---
+++ database/factories/JobFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Business;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Job>
+ */
+class JobFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->jobTitle(),
+            'description' => "Descrizione di un'offerta di lavoro.",
+            'salary' => "$50.000 USD",
+            'status' => 'Attivo',
+            'business_id' => 1
+        ];
+    }
+}
