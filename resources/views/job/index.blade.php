f542ad3151ae2afe5ad7d323a3c52af9f2c12342
Pasqualecannavacciuolo <p.cannavacciuolo@icloud.com>
2024-10-29 19:19:18 +0100

    Aggiornamento

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,27 +1,34 @@
 <x-layout>
     <div class="flex flex-col gap-5 p-4 lg:p-7">
         <h1 class="p-1 text-xl font-bold">Naviga tra le offerte</h1>
-        <div class="flex flex-col gap-5">
+
+        <!-- Wrapper griglia -->
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5"> <!-- Griglia responsive -->
             <!-- JOB OFFER -->
             @foreach ($jobs as $job)
-                <div class="flex flex-col w-full bg-zinc-50 rounded-md shadow-md">
+                <div class="flex flex-col bg-sky-50/10 rounded-md shadow-md min-h-[200px] max-w-[300px] w-full mx-auto">
+                    <!-- Larghezza fissa per uniformità -->
                     <div class="flex flex-row p-4 justify-between">
                         <div class="flex flex-col gap-1">
-                            <p class="text-xs text-zinc-500">{{ $job->business->name }}</p>
-                            <h1 class="font-semibold">{{ $job->title }}</h1>
+                            <!-- BUSINESS IMAGE -->
+                            <div class="w-10 h-auto">
+                                {!! str_replace(['width="144px"', 'height="144px"'], ['width="100%"', 'height="100%"'], $job->business->image) !!}
+                            </div>
                         </div>
                         <div>
                             <x-badges.badge>{{ $job->status }}</x-badges.badge>
                         </div>
                     </div>
-                    <div class="p-4">
-                        <p>
-                            {{ $job->description }}
-                        </p>
+                    <div class="flex flex-col p-4 flex-grow">
+                        <!-- BUSINESS NAME -->
+                        <p class="text-xs text-zinc-500">{{ $job->business->name }}</p>
+
+                        <!-- JOB TITLE -->
+                        <h1 class="font-semibold">{{ $job->title }}</h1>
                     </div>
                     <div class="flex flex-row p-4 justify-between">
                         <div class="flex flex-col gap-1">
-                            <p class="text-md">{{ $job->salary }}</p>
+                            <x-badges.info>{{ $job->salary }}</x-badges.info>
                         </div>
                         <div>
                             <x-navigation.link-btn href="/jobs/{{ $job->id }}">Scopri</x-navigation.link-btn>
@@ -30,6 +37,7 @@
                 </div>
             @endforeach
         </div>
+
         <div>
             {{ $jobs->links() }}
         </div>
