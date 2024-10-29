aa33d5d9070188d79143deef073c5b45ece13408
Pasqualecannavacciuolo <p.cannavacciuolo@icloud.com>
2024-10-29 19:18:47 +0100

    Aggiunta campo

--- database/migrations/2024_10_29_121106_create_businesses_table.php
+++ database/migrations/2024_10_29_121106_create_businesses_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
         Schema::create('businesses', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->text('image');
             $table->foreignIdFor(User::class);
             $table->timestamps();
         });
