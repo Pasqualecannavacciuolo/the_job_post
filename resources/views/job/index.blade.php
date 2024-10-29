4a64fe8e18404a0caca925c2722a885a844154bb
Pasqualecannavacciuolo <p.cannavacciuolo@icloud.com>
2024-10-29 15:54:02 +0100

    Creata view per visualizzare la lista dei job

---
+++ resources/views/job/index.blade.php
@@ -0,0 +1,37 @@
+<x-layout>
+    <div class="flex flex-col gap-5">
+        <h1 class="p-1 text-xl font-bold">Naviga tra le offerte</h1>
+        <div class="flex flex-col gap-5">
+            <!-- JOB OFFER -->
+            @foreach ($jobs as $job)
+                <div class="flex flex-col w-full bg-zinc-50">
+                    <div class="flex flex-row p-5 justify-between">
+                        <div class="flex flex-col gap-1">
+                            <p class="text-xs text-zinc-500">{{ $job->business->name }}</p>
+                            <h1 class="font-semibold">{{ $job->title }}</h1>
+                        </div>
+                        <div>
+                            <x-badges.badge>{{ $job->status }}</x-badges.badge>
+                        </div>
+                    </div>
+                    <div class="p-5">
+                        <p>
+                            {{ $job->description }}
+                        </p>
+                    </div>
+                    <div class="flex flex-row p-5 justify-between">
+                        <div class="flex flex-col gap-1">
+                            <p class="text-md">{{ $job->salary }}</p>
+                        </div>
+                        <div>
+                            <x-buttons.default-button>Scopri</x-buttons.default-button>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+        <div>
+            {{ $jobs->links() }}
+        </div>
+    </div>
+</x-layout>
