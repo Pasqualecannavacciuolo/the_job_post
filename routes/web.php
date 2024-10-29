653c804090fdcda1a746ac15485eed87d1cc24f5
Pasqualecannavacciuolo <p.cannavacciuolo@icloud.com>
2024-10-29 15:54:16 +0100

    Aggiunto endpoint per vedere la lista dei job

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
@@ -14,3 +15,7 @@ Route::post('/register', [RegisteredUserController::class, 'register']);
 Route::get('/login', [SessionController::class, 'show']);
 Route::post('/login', [SessionController::class, 'login']);
 Route::post('/logout', [SessionController::class, 'logout']);
+
+
+// Job
+Route::get('/jobs', [JobController::class, 'show']);
