51782c32c83e30f473e0e7058291622fc5685b81
Pasqualecannavacciuolo <p.cannavacciuolo@icloud.com>
2024-10-29 15:53:22 +0100

    Creati i Model

---
+++ app/Models/Job.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Tag;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model
+{
+    use HasFactory;
+
+    // Override del nome della tabella di default
+    protected $table = 'job_listings';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'salary',
+        'status',
+        'business_id',
+    ];
+
+    public function business()
+    {
+        return $this->belongsTo(Business::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, foreignPivotKey: "job_listing_id");
+    }
+}
