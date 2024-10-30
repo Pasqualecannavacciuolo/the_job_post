5b796c978a597ce7fab099b8d0b87cae0ba9e151
Pasqualecannavacciuolo <p.cannavacciuolo@icloud.com>
2024-10-30 17:52:31 +0100

    Aggiunto alert errore

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,6 +7,26 @@
                 <h2 class="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Log in</h2>
             </div>
 
+            @error('attempt')
+                <div role="alert" class="w-72 lg:w-96 mt-5 mx-auto">
+                    <div role="alert" class="mt-3 relative flex w-full p-3 text-sm text-white bg-red-500 rounded-md"
+                        id="alert">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-5 h-5 mr-2">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0M3.124 7.5A8.969 8.969 0 0 1 5.292 3m13.416 0a8.969 8.969 0 0 1 2.168 4.5" />
+                        </svg>
+
+                        {{ $message }}
+                        <button onclick="closeAlert()"
+                            class="flex items-center justify-center transition-all px-2 h-8 rounded-md bg-white text-black absolute top-1.5 right-1.5"
+                            type="button">
+                            Close
+                        </button>
+                    </div>
+                </div>
+            @enderror
+
             <div class="sm:mx-auto sm:w-full sm:max-w-xl flex flex-col items-center">
                 <!-- Form -->
                 <form class="w-72 lg:w-96 space-y-6" action="/login" method="POST">
@@ -42,4 +62,12 @@
             </div>
         </div>
     </div>
+
+    <!-- Script per permettere la chiusura dell'alert -->
+    <script>
+        function closeAlert() {
+            const alertElement = document.getElementById('alert');
+            alertElement.style.display = 'none'; // Hides the alert
+        }
+    </script>
 </x-layout>
