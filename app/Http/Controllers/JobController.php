66837350404ece15d40bb6db32638722e12ce822
Pasqualecannavacciuolo <p.cannavacciuolo@icloud.com>
2024-10-30 13:47:57 +0100

    Aggiunte funzioni per effettuare l'edit di un job

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -13,9 +13,28 @@ class JobController extends Controller
         return view('job.index', ['jobs' => $jobs]);
     }
 
-    public function show_details(int $id): View
+    public function show_details(Job $job): View
     {
-        $job = Job::with('business')->findOrFail($id);
+        //$job = Job::with('business')->findOrFail($id);
         return view('job.details', ['job' => $job]);
+    }
+
+    public function show_edit(Job $job): View
+    {
+        return view('job.edit', ['job' => $job]);
+    }
+
+    public function edit_job(Job $job)
+    {
+        // Validate input
+        $validatedAttributes = request()->validate([
+            'title' => ['required', 'max:50'],
+            'description' => ['required'],
+            'salary' => ['required']
+        ]);
+        // Register changes in Database
+        $job->update($validatedAttributes);
+        // Redirect
+        return redirect('/jobs/' . $job->id);
     }
 }
